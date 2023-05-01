4d26898d13f50ff71b7fbc8470620b0ec99c3614
Shikhar Rai <tonyrai426@gmail.com>
2023-05-01 22:26:20 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,39 +6,14 @@ export default function Login() {
       <div class="form signup">
         <header>Signup</header>
         <form action="#">
-          <input type="text" placeholder="Full name" required />
+          <input type="text" placeholder="Full Name" required />
+          <input type="text" placeholder="Parent's Name" required />
+          <input type="date" placeholder="Date of Birth" required />
           <input type="number" placeholder="Phone Number" required />
-          <input type="password" placeholder="Password" required />
-          <div class="checkbox">
-            <input type="checkbox" id="signupCheck" />
-            <label for="signupCheck">I accept all terms & conditions</label>
-          </div>
-          <input type="submit" value="Signup" />
-        </form>
-      </div>
-
-      <div class="form login">
-        <header>Login</header>
-        <form action="#">
-          <input type="number" placeholder="Phone Number" required />
-          <input type="password" placeholder="Password" required />
-          <a href="#">Forgot password?</a>
+          
           <input type="submit" value="Login" />
         </form>
       </div>
-
-      {/* <script>
-        const wrapper = document.querySelector(".wrapper"),
-          signupHeader = document.querySelector(".signup header"),
-          loginHeader = document.querySelector(".login header");
-
-        loginHeader.addEventListener("click", () => {
-          wrapper.classList.add("active");
-        });
-        signupHeader.addEventListener("click", () => {
-          wrapper.classList.remove("active");
-        });
-      </script> */}
     </section>
   )
 }
