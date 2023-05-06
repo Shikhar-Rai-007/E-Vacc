ddf530c22606a2c5b50ba5824c4dce2f500c55bd
Shikhar Rai <tonyrai426@gmail.com>
2023-05-07 00:51:12 +0530

    Final Commit of The Day

--- src/Components/Alogin.js
+++ src/Components/Alogin.js
@@ -5,10 +5,8 @@ function Alogin() {
     <div class="wrapper">
       <div class="form signup">
         <header>Signup</header>
-        <form method="POST" action="/user">
+        <form method="POST" action="/admin">
           <input type="text" placeholder="Full Name" required />
-          {/* <input type="text" placeholder="Parent's Name" required /> */}
-          {/* <input type="date" placeholder="Date of Birth" required /> */}
           <input type="number" placeholder="Phone Number" required />
           
           <input type="submit" value="Login" />
