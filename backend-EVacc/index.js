d760f5f538ebaa73b03d13f462472ae8945e23e2
Shikhar Rai <tonyrai426@gmail.com>
2023-05-06 23:17:32 +0530

    Commit

--- backend-EVacc/index.js
+++ backend-EVacc/index.js
@@ -1,1 +1,15 @@
-const mongoose=require('mongoose');
+const connectToMongo=require('../backend-EVacc/DBConnection');
+connectToMongo();
+
+const express=require('express');
+
+const app=express();
+const port=3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(port,()=>{
+    console.log(`Listening to port:${port}`);
+})
