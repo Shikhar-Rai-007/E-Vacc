c85c66a951e2298d1c04b0a647927a730962efb2
Shikhar Rai <tonyrai426@gmail.com>
2023-05-09 11:56:14 +0530

    Update Admin.js

--- backend-EVacc/routes/Admin.js
+++ backend-EVacc/routes/Admin.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const Admins=require('../models/Admins');
+const Users = require('../models/Users');
 const router=express.Router();
 
 
@@ -10,5 +11,24 @@ router.get('/',(req,res)=>{
     res.send(req.body);
 })
 
+router.get('/getAllUsers',async (req,res)=>{
+    const users=await Users.find();
+    res.json(users);
+})
 
+router.post('/createUsers',async(req,res)=>{
+    try{
+        const{firstName,lastName,phNo,age}=req.body;
+        const user=new Users({
+            firstName,lastName,phNo,age
+        })
+
+        const savedUser=await user.save();
+        res.json(savedUser);
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).send("Some Error Occured");
+    }
+})
 module.exports=router;
