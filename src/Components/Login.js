fa8ac7deabb1dce89b821df58b0ae3f894dab0e2
Shikhar Rai <tonyrai426@gmail.com>
2023-05-11 14:32:05 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Login.css';
 export default function Login() {
+
+  const handleInputChange=(event)=>{
+    const val=(event.target.value);
+    console.log(val);
+  }
   return (
     <div class="wrapper">
       <div class="form signup">
         <header>Signup</header>
-        <form onSubmit={handleRequest}>
+        <form> 
           {/* <input type="text" placeholder="Full Name" required />
           <input type="text" placeholder="Parent's Name" required />
           <input type="date" placeholder="Date of Birth" required /> */}
-          <input type="number" placeholder="Phone Number" required />
+          <input type="number" placeholder="Phone Number" onChange={handleInputChange} required />
           
           <input type="submit" value="See Vaccination Status" />
         </form>
