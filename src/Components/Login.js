fb4ecb5b20c69f0156817f7abeacdadd8f53229f
Shikhar Rai <tonyrai426@gmail.com>
2023-05-11 11:44:04 +0530

    Some changes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,13 +5,13 @@ export default function Login() {
     <div class="wrapper">
       <div class="form signup">
         <header>Signup</header>
-        <form method="POST" action="/user">
-          <input type="text" placeholder="Full Name" required />
+        <form onSubmit={handleRequest}>
+          {/* <input type="text" placeholder="Full Name" required />
           <input type="text" placeholder="Parent's Name" required />
-          <input type="date" placeholder="Date of Birth" required />
+          <input type="date" placeholder="Date of Birth" required /> */}
           <input type="number" placeholder="Phone Number" required />
           
-          <input type="submit" value="Login" />
+          <input type="submit" value="See Vaccination Status" />
         </form>
       </div>
     </div>
